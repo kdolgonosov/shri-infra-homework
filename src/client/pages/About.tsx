b93458cf6375f325c8ad53069490d482cdcabfd4
kdolgonosov <89645232+kdolgonosov@users.noreply.github.com>
2024-07-21 12:10:48 +0300

    fix

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
             <div className='row'>
                 <div className='col'>
                     <h1>About</h1>
-                    <p>Author: akdolgonosov</p>
+                    <p>Author: kdolgonosov</p>
                 </div>
             </div>
         </div>
