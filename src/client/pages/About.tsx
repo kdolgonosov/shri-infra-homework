d899d6cb9d124746a387558a95b3ab485f40f33a
kdolgonosov <89645232+kdolgonosov@users.noreply.github.com>
2024-07-21 12:01:06 +0300

    fix author

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
             <div className='row'>
                 <div className='col'>
                     <h1>About</h1>
-                    <p>Author: tochno ne kdolgonosov</p>
+                    <p>Author: kdolgonosov</p>
                 </div>
             </div>
         </div>
